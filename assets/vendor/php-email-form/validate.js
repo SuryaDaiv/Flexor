57e84773f17aef771defe2ee340569a6fc3bb3e5
Surya Veera Reddy Sirigireddy <suryaveera96@gmail.com>
2023-05-21 20:30:15 -0500

    updated to redirect to success page on submission

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -49,6 +49,32 @@
     });
   });
 
+  // function php_email_form_submit(thisForm, action, formData) {
+  //   fetch(action, {
+  //     method: 'POST',
+  //     body: formData,
+  //     headers: {'X-Requested-With': 'XMLHttpRequest'}
+  //   })
+  //   .then(response => {
+  //     if( response.ok ) {
+  //       return response.text();
+  //     } else {
+  //       throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
+  //     }
+  //   })
+  //   .then(data => {
+  //     thisForm.querySelector('.loading').classList.remove('d-block');
+  //     if (data.trim() == 'OK') {
+  //       thisForm.querySelector('.sent-message').classList.add('d-block');
+  //       thisForm.reset(); 
+  //     } else {
+  //       throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     displayError(thisForm, error);
+  //   });
+  // }
   function php_email_form_submit(thisForm, action, formData) {
     fetch(action, {
       method: 'POST',
@@ -65,8 +91,9 @@
     .then(data => {
       thisForm.querySelector('.loading').classList.remove('d-block');
       if (data.trim() == 'OK') {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset(); 
+        // thisForm.querySelector('.sent-message').classList.add('d-block');
+        // thisForm.reset(); 
+        window.location.href = 'contact-success-page.html'; // Redirecting to the success page
       } else {
         throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
       }
@@ -74,7 +101,8 @@
     .catch((error) => {
       displayError(thisForm, error);
     });
-  }
+}
+
 
   function displayError(thisForm, error) {
     thisForm.querySelector('.loading').classList.remove('d-block');
