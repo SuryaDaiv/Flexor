25626de166a9cd548924cff0f6a8180a34a7817f
Surya Veera Reddy Sirigireddy <suryaveera96@gmail.com>
2023-05-21 21:08:32 -0500

    commented validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,143 +1,143 @@
-/**
-* PHP Email Form Validation - v3.6
-* URL: https://bootstrapmade.com/php-email-form/
-* Author: BootstrapMade.com
-*/
-(function () {
-  "use strict";
+// /**
+// * PHP Email Form Validation - v3.6
+// * URL: https://bootstrapmade.com/php-email-form/
+// * Author: BootstrapMade.com
+// */
+// (function () {
+//   "use strict";
 
-  let forms = document.querySelectorAll('.php-email-form');
+//   let forms = document.querySelectorAll('.php-email-form');
 
-  forms.forEach( function(e) {
-    e.addEventListener('submit', function(event) {
-      event.preventDefault();
+//   forms.forEach( function(e) {
+//     e.addEventListener('submit', function(event) {
+//       event.preventDefault();
 
-      let thisForm = this;
+//       let thisForm = this;
 
-      let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+//       let action = thisForm.getAttribute('action');
+//       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
       
-      if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!');
-        return;
-      }
-      thisForm.querySelector('.loading').classList.add('d-block');
-      thisForm.querySelector('.error-message').classList.remove('d-block');
-      thisForm.querySelector('.sent-message').classList.remove('d-block');
-
-      let formData = new FormData( thisForm );
-
-      if ( recaptcha ) {
-        if(typeof grecaptcha !== "undefined" ) {
-          grecaptcha.ready(function() {
-            try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
-              .then(token => {
-                formData.set('recaptcha-response', token);
-                php_email_form_submit(thisForm, action, formData);
-              })
-            } catch(error) {
-              displayError(thisForm, error);
-            }
-          });
-        } else {
-          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
-        }
-      } else {
-        php_email_form_submit(thisForm, action, formData);
-      }
-    });
-  });
-
-  // function php_email_form_submit(thisForm, action, formData) {
-  //   fetch(action, {
-  //     method: 'POST',
-  //     body: formData,
-  //     headers: {'X-Requested-With': 'XMLHttpRequest'}
-  //   })
-  //   .then(response => {
-  //     if( response.ok ) {
-  //       return response.text();
-  //     } else {
-  //       throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
-  //     }
-  //   })
-  //   .then(data => {
-  //     thisForm.querySelector('.loading').classList.remove('d-block');
-  //     if (data.trim() == 'OK') {
-  //       thisForm.querySelector('.sent-message').classList.add('d-block');
-  //       thisForm.reset(); 
-  //     } else {
-  //       throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-  //     }
-  //   })
-  //   .catch((error) => {
-  //     displayError(thisForm, error);
-  //   });
-  // }
-
-  // Redirecting to the success page trial 2
-//   function php_email_form_submit(thisForm, action, formData) {
-//     fetch(action, {
-//       method: 'POST',
-//       body: formData,
-//       headers: {'X-Requested-With': 'XMLHttpRequest'}
-//     })
-//     .then(response => {
-//       if( response.ok ) {
-//         return response.text();
-//       } else {
-//         throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
+//       if( ! action ) {
+//         displayError(thisForm, 'The form action property is not set!');
+//         return;
 //       }
-//     })
-//     .then(data => {
-//       thisForm.querySelector('.loading').classList.remove('d-block');
-//       if (data.trim() == 'OK') {
-//         // thisForm.querySelector('.sent-message').classList.add('d-block');
-//         // thisForm.reset(); 
-//         window.location.href = 'contact-success-page.html'; // Redirecting to the success page
+//       thisForm.querySelector('.loading').classList.add('d-block');
+//       thisForm.querySelector('.error-message').classList.remove('d-block');
+//       thisForm.querySelector('.sent-message').classList.remove('d-block');
+
+//       let formData = new FormData( thisForm );
+
+//       if ( recaptcha ) {
+//         if(typeof grecaptcha !== "undefined" ) {
+//           grecaptcha.ready(function() {
+//             try {
+//               grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
+//               .then(token => {
+//                 formData.set('recaptcha-response', token);
+//                 php_email_form_submit(thisForm, action, formData);
+//               })
+//             } catch(error) {
+//               displayError(thisForm, error);
+//             }
+//           });
+//         } else {
+//           displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
+//         }
 //       } else {
-//         throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+//         php_email_form_submit(thisForm, action, formData);
 //       }
-//     })
-//     .catch((error) => {
-//       displayError(thisForm, error);
 //     });
+//   });
+
+//   // function php_email_form_submit(thisForm, action, formData) {
+//   //   fetch(action, {
+//   //     method: 'POST',
+//   //     body: formData,
+//   //     headers: {'X-Requested-With': 'XMLHttpRequest'}
+//   //   })
+//   //   .then(response => {
+//   //     if( response.ok ) {
+//   //       return response.text();
+//   //     } else {
+//   //       throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
+//   //     }
+//   //   })
+//   //   .then(data => {
+//   //     thisForm.querySelector('.loading').classList.remove('d-block');
+//   //     if (data.trim() == 'OK') {
+//   //       thisForm.querySelector('.sent-message').classList.add('d-block');
+//   //       thisForm.reset(); 
+//   //     } else {
+//   //       throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+//   //     }
+//   //   })
+//   //   .catch((error) => {
+//   //     displayError(thisForm, error);
+//   //   });
+//   // }
+
+//   // Redirecting to the success page trial 2
+// //   function php_email_form_submit(thisForm, action, formData) {
+// //     fetch(action, {
+// //       method: 'POST',
+// //       body: formData,
+// //       headers: {'X-Requested-With': 'XMLHttpRequest'}
+// //     })
+// //     .then(response => {
+// //       if( response.ok ) {
+// //         return response.text();
+// //       } else {
+// //         throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
+// //       }
+// //     })
+// //     .then(data => {
+// //       thisForm.querySelector('.loading').classList.remove('d-block');
+// //       if (data.trim() == 'OK') {
+// //         // thisForm.querySelector('.sent-message').classList.add('d-block');
+// //         // thisForm.reset(); 
+// //         window.location.href = 'contact-success-page.html'; // Redirecting to the success page
+// //       } else {
+// //         throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+// //       }
+// //     })
+// //     .catch((error) => {
+// //       displayError(thisForm, error);
+// //     });
+// // }
+
+// function php_email_form_submit(thisForm, action, formData) {
+//   fetch(action, {
+//     method: 'POST',
+//     body: formData,
+//     headers: {'X-Requested-With': 'XMLHttpRequest'}
+//   })
+//   .then(response => {
+//     if( response.ok ) {
+//       return response.text();
+//     } else {
+//       throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
+//     }
+//   })
+//   .then(data => {
+//     thisForm.querySelector('.loading').classList.remove('d-block');
+//     if (data.trim() == 'OK') {
+//       thisForm.querySelector('.sent-message').classList.add('d-block');
+//       thisForm.reset(); 
+//       window.location.href = 'contact-success-page.html'; // Redirecting to the success page
+//     } else {
+//       throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+//     }
+//   })
+//   .catch((error) => {
+//     displayError(thisForm, error);
+//     window.location.href = 'contact-success-page.html'; // Redirecting to the success page even if there is an error
+//   });
 // }
 
-function php_email_form_submit(thisForm, action, formData) {
-  fetch(action, {
-    method: 'POST',
-    body: formData,
-    headers: {'X-Requested-With': 'XMLHttpRequest'}
-  })
-  .then(response => {
-    if( response.ok ) {
-      return response.text();
-    } else {
-      throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
-    }
-  })
-  .then(data => {
-    thisForm.querySelector('.loading').classList.remove('d-block');
-    if (data.trim() == 'OK') {
-      thisForm.querySelector('.sent-message').classList.add('d-block');
-      thisForm.reset(); 
-      window.location.href = 'contact-success-page.html'; // Redirecting to the success page
-    } else {
-      throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-    }
-  })
-  .catch((error) => {
-    displayError(thisForm, error);
-    window.location.href = 'contact-success-page.html'; // Redirecting to the success page even if there is an error
-  });
-}
+//   function displayError(thisForm, error) {
+//     thisForm.querySelector('.loading').classList.remove('d-block');
+//     thisForm.querySelector('.error-message').innerHTML = error;
+//     thisForm.querySelector('.error-message').classList.add('d-block');
+//   }
 
-  function displayError(thisForm, error) {
-    thisForm.querySelector('.loading').classList.remove('d-block');
-    thisForm.querySelector('.error-message').innerHTML = error;
-    thisForm.querySelector('.error-message').classList.add('d-block');
-  }
-
-})();
+// })();
